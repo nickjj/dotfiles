041e9b662433d6f7f5a591003445a5c3c2e6d0fe
Nick Janetakis <nick.janetakis@gmail.com>
2025-03-20 16:22:52 -0400

    Fix comma with period in comment.

--- .config/nvim/after/queries/gitcommit/highlights.scm
+++ .config/nvim/after/queries/gitcommit/highlights.scm
@@ -1,6 +1,6 @@
 ;; extends
 
-; Highlight longer lines as a suggestion to consider using less characters,
+; Highlight longer lines as a suggestion to consider using less characters.
 ((subject) @comment.warning
   (#vim-match? @comment.warning ".\{50,}")
   (#offset! @comment.warning 0 50 0 0))
