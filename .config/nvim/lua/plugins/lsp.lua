5f1693e16964e8695bda7001797597117cb421a4
Nick Janetakis <nick.janetakis@gmail.com>
2025-03-27 10:08:53 -0400

    Add CloudFormation YAML tags
    Without these, if you have any CloudFormation or SAM templates then
    you'll get a lot of diagnostic errors. This modifies the YAML LSP to
    have an awareness of them and be able to format them too.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,6 +11,32 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        yamlls = {
+          settings = {
+            yaml = {
+              customTags = {
+                "!And sequence",
+                "!Base64",
+                "!Cidr sequence",
+                "!Equals sequence",
+                "!FindInMap sequence",
+                "!GetAZs",
+                "!GetAtt",
+                "!If sequence",
+                "!ImportValue",
+                "!Join sequence",
+                "!Length sequence",
+                "!Not sequence",
+                "!Or sequence",
+                "!Ref",
+                "!Select sequence",
+                "!Split sequence",
+                "!Sub",
+                "!Transform scalar",
+              },
+            },
+          },
+        },
         bashls = {
           settings = {
             filetypes = { "sh", "bash", "zsh" },
