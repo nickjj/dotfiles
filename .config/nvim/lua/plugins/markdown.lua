7ceb38a1373619dbd239942f112d4706ec65d3b6
Nick Janetakis <nick.janetakis@gmail.com>
2025-03-31 19:03:11 -0400

    Try extended Markdown rendering

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,44 @@
+local data_path = vim.fn.stdpath("config") .. "/lua/plugins/data"
+local markdownlint_cli2_path = data_path .. "/.markdownlint-cli2.yaml"
+local markdown_preview_css_path = data_path .. "/github-markdown.css"
+
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    init = function()
+      vim.g.mkdp_auto_close = false
+      vim.g.mkdp_markdown_css = markdown_preview_css_path
+    end,
+    build = "cd app && yarn install",
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        ["markdownlint-cli2"] = {
+
+          args = { "--config", markdownlint_cli2_path, "--fix", "$FILENAME" },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", markdownlint_cli2_path, "--" },
+        },
+      },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        width = "block",
+        min_width = vim.o.colorcolumn,
+      },
+    },
+  },
+}
