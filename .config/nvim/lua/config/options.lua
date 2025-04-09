e5051f2eaf9d382e0a4acc3bdf51406511ec325b
Nick janetakis <nick.janetakis@gmail.com>
2025-04-09 05:39:45 -0400

    Scope local options to variable
    This will make the file easier to skim as we introduce more option
    types.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,17 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup.
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
+local opt = vim.opt
+
 -- I prefer seeing all characters by default.
-vim.opt.conceallevel = 0
+opt.conceallevel = 0
 
 -- Show a vertical line at this character.
-vim.opt.colorcolumn = "80"
+opt.colorcolumn = "80"
 
 -- Each buffer gets its own status line instead of sharing one.
-vim.opt.laststatus = 2
+opt.laststatus = 2
 
 -- These are all invisible by default but we can toggle them with a keymap.
-vim.opt.listchars = {
+opt.listchars = {
   eol = "$",
   tab = ">-",
   trail = "-",
@@ -21,13 +23,13 @@ vim.opt.listchars = {
   conceal = "+",
   nbsp = "&",
 }
-vim.opt.list = false
+opt.list = false
 
 -- Don't auto-scroll N number of lines from the top of the buffer.
-vim.opt.scrolloff = 0
+opt.scrolloff = 0
 
 -- Allow left and right arrow keys to move to the previous and next line.
-vim.opt.whichwrap = "b,s,<,>"
+opt.whichwrap = "b,s,<,>"
 
 -- Wrap lines so it's easier to see anything that's cut off.
-vim.opt.wrap = true
+opt.wrap = true
