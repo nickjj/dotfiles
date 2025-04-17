b2136f18daf8e5aaefada3c1a32b62d0e8e9be7d
Nick janetakis <nick.janetakis@gmail.com>
2025-04-17 06:53:34 -0400

    Document how to use a right aligned explorer
    Some folks prefer this and it wasn't easy to find how to do this in the
    docs. The reason I don't use a right aligned explorer is because then
    the explorer is very very far away from your code when you have a large
    screen resolution and a full size terminal window.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -15,7 +15,15 @@ return {
           -- Explorer and the rest of the sources don't define their own opts
           -- so it will use the picker options defined above and we can choose
           -- to override them if desired.
-          explorer = { ignored = true },
+          explorer = {
+            ignored = true,
+            -- If you prefer a right aligned explorer, uncomment this:
+            -- layout = {
+            --   layout = {
+            --     position = "right",
+            --   },
+            -- },
+          },
         },
       },
       terminal = {
