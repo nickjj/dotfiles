cb15fd6b826c01d3f49d3e71bc51b6e941248d9c
Nick Janetakis <nick.janetakis@gmail.com>
2025-04-24 11:30:38 -0400

    Add word count and read time to Markdown files

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,28 @@
+local function wordcount()
+  local label = "word"
+  local word_count = 0
+
+  if vim.fn.mode():find("[vV]") then
+    word_count = vim.fn.wordcount().visual_words
+  else
+    word_count = vim.fn.wordcount().words
+  end
+
+  if word_count > 1 then
+    label = label .. "s"
+  end
+
+  return word_count .. " " .. label
+end
+
+local function readingtime()
+  -- 200 is about the average words read per minute.
+  return tostring(math.ceil(vim.fn.wordcount().words / 200.0)) .. " min"
+end
+
+local function is_prose()
+  return vim.bo.filetype == "markdown" or vim.bo.filetype == "text"
+end
 return {
   {
     "nvim-lualine/lualine.nvim",
@@ -5,6 +30,11 @@ return {
       sections = {
         -- Disable the clock.
         lualine_z = {},
+        -- Disable the default clock and replace it with word stats.
+        lualine_z = {
+          { wordcount, cond = is_prose },
+          { readingtime, cond = is_prose },
+        },
       },
     },
   },
