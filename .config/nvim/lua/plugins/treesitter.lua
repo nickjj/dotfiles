b9208ba46289bab62e4c117b5e8dfca192b03ebc
Nick Janetakis <nick.janetakis@gmail.com>
2025-05-03 07:24:02 -0400

    Use opts_extend to simplify ensure_installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,16 @@ return {
     dependencies = {
       "bezhermoso/tree-sitter-ghostty",
     },
-    opts = function(_, opts)
+    opts_extend = { "ensure_installed" },
+    opts = {
+      -- Identation was often misgaligned, especially with list items. This
+      -- lets Neovim handle indentation directly. This is a bug with Treesitter
+      -- and should be fixed in 1.0.
+      indent = {
+        disable = { "yaml" },
+      },
       -- Add custom treesitters not present by default in LazyVim.
-      vim.list_extend(opts.ensure_installed, {
+      ensure_installed = {
         "csv",
         "dockerfile",
         "elixir",
@@ -23,12 +30,7 @@ return {
         "ruby",
         "terraform",
         "toml",
-      })
-
-      -- Identation was often misgaligned, especially with list items. This
-      -- lets Neovim handle indentation directly. This is a bug with Treesitter
-      -- and should be fixed in 1.0.
-      opts.indent.disable = { "yaml" }
-    end,
+      },
+    },
   },
 }
