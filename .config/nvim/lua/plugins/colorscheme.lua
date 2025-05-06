b828b51087656711ca0ad02b7497327f5aeff6d6
Nick Janetakis <nick.janetakis@gmail.com>
2025-05-06 08:23:37 -0400

    Adjust Snacks colors for Tokyonight Moon
    This makes things a bit easier to read.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -24,6 +24,10 @@ return {
         keywords = { italic = false },
       },
       on_highlights = function(hl, c)
+        hl.SnacksPickerPathHidden = { fg = c.comment }
+        hl.SnacksPickerDir = { fg = c.comment }
+        hl.SnacksPickerGitStatusIgnored = { fg = c.nontext }
+        hl.SnacksPickerGitStatusUntracked = { fg = c.nontext }
         hl.ColorColumn = { bg = c.bg_highlight }
         hl.SpellBad = { fg = c.red, undercurl = true }
         hl.SpellCap = { fg = c.red, undercurl = true }
