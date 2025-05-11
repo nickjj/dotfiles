8a2a9a70a63401c83c75d4edbdaed33895f47f23
Nick Janetakis <nick.janetakis@gmail.com>
2025-05-11 19:55:32 -0400

    Lock Mason to 1.X until LazyVim supports 2.X

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,9 @@
 return {
+  -- Temporarily lock mason related plugins to 1.X until LazyVim supports 2.X.
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   {
     "williamboman/mason.nvim",
+    version = "^1.0.0",
     opts = {
       ensure_installed = {
         "shellcheck",
