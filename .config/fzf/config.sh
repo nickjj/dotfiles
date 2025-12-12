544e7939ff80aea414837473317789da3f92e5fc
Nick Janetakis <nick.janetakis@gmail.com>
2025-12-12 08:41:12 -0500

    Spruce up fzf's default preview options

--- .config/fzf/config.sh
+++ .config/fzf/config.sh
@@ -1,6 +1,6 @@
 export FZF_DEFAULT_COMMAND="rg --files --follow --hidden --glob '!.git'"
-export FZF_DEFAULT_OPTS="--highlight-line --info=inline-right --ansi --layout=reverse --border=none"
-export FZF_CTRL_T_OPTS="--preview='less {}' --height=100% --bind shift-up:preview-page-up,shift-down:preview-page-down"
+export FZF_DEFAULT_OPTS="--highlight-line --info=inline-right --ansi --layout=reverse --border=none --bind shift-up:preview-page-up,shift-down:preview-page-down"
+export FZF_CTRL_T_OPTS="--preview='bat --color=always {}' --height=100%"
 
 # shellcheck disable=SC1091
 . "${XDG_CONFIG_HOME}/fzf/theme.sh"
