68718d9de8f7ff7b434209b2625e6f70eef3323d
Nick Janetakis <nick.janetakis@gmail.com>
2026-04-10 12:58:58 -0400

    Remove unnecessary default fzf options

--- .config/fzf/config.sh
+++ .config/fzf/config.sh
@@ -1,5 +1,5 @@
 export FZF_DEFAULT_COMMAND="rg --files --follow --hidden --glob '!.git'"
-export FZF_DEFAULT_OPTS="--highlight-line --info=inline-right --ansi --layout=reverse --border=none --bind shift-up:preview-page-up,shift-down:preview-page-down"
+export FZF_DEFAULT_OPTS="--ansi --info=inline-right --layout=reverse --border=none --bind shift-up:preview-page-up,shift-down:preview-page-down"
 export FZF_CTRL_T_OPTS="--height=100% --preview='bat --color=always {}'"
 
 # shellcheck disable=SC1091
